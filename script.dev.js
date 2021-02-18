f6fd2c04593af9ae23c7b94769ef61366a5cbd02
Ashlee <ashleeryan@outlook.com>
2021-02-18 20:55:59 +0000

    added lose conditions

--- script.dev.js
+++ script.dev.js
@@ -4,18 +4,29 @@ var container = document.getElementById("container");
 var board = [];
 var snakePosVal;
 var score = 0;
+var numArr = [];
+
+for (var i = 1; i < 784; i++) {
+  if (i % 28 === 0 || i % 28 === 27 || i <= 27 || i > 756 && i <= 784) {
+    continue;
+  }
+
+  numArr.push(i);
+}
+
+console.log(numArr);
 var scoreSelector = document.querySelector("#score");
 
 var createGrid = function createGrid() {
-  for (var i = 0; i < 784; i++) {
+  for (var _i = 0; _i < 784; _i++) {
     var grid = document.createElement('div');
     container.appendChild(grid);
     board.push(grid);
 
-    if (i <= 27 || i % 28 == 0 || i > 756 && i <= 784 || i % 28 === 27) {
-      board[i].classList.add('wall');
+    if (_i <= 27 || _i % 28 === 0 || _i > 756 && _i <= 784 || _i % 28 === 27) {
+      board[_i].classList.add('wall');
     } else {
-      board[i].classList.add('bg');
+      board[_i].classList.add('bg');
     }
   }
 };
@@ -25,36 +36,44 @@ console.log(board);
 var snakePos = 350;
 board[snakePos].classList.add('snake');
 
+var endGameWall = function endGameWall() {
+  if (board[snakePos].classList.contains("wall")) {
+    alert("game over");
+  }
+};
+
 var moveSnake = function moveSnake(event) {
   // board[snakePos].classList.remove('snake')
   var down = function down() {
     board[snakePos].classList.remove('snake');
     snakePos = snakePos + snakePosVal;
     board[snakePos].classList.add('snake');
+    eatFood();
+    endGameWall();
   };
 
   if (event.keyCode === 40) {
     snakePosVal = 28;
     console.log('down');
-    var snakePosInterval = setInterval(down, 300);
+    var snakePosInterval = setInterval(down, 200);
     return clearInterval(snakePosInterval);
   } else if (event.keyCode === 13) {
     snakePosVal = 1;
-    snakePosInterval = setInterval(down, 300);
+    snakePosInterval = setInterval(down, 200);
   } else if (event.keyCode === 39) {
     clearInterval(snakePosInterval);
     snakePosVal = 1;
-    snakePosInterval = setInterval(down, 300);
+    snakePosInterval = setInterval(down, 200);
     return clearInterval(snakePosInterval);
   } else if (event.keyCode === 38) {
     console.log('up');
     snakePosVal = -28;
-    snakePosInterval = setInterval(down, 300);
+    snakePosInterval = setInterval(down, 200);
     return clearInterval(snakePosInterval);
   } else if (event.keyCode === 37) {
     console.log('left');
     snakePosVal = -1;
-    snakePosInterval = setInterval(down, 300);
+    snakePosInterval = setInterval(down, 200);
     return clearInterval(snakePosInterval);
   }
 
@@ -70,13 +89,28 @@ var drawFood = function drawFood(event) {
   }
 };
 
+var dropBomb = function dropBomb() {
+  board[snakePos].classList.add("tail");
+};
+
 var eatFood = function eatFood(event) {
-  if (event.keyCode === 32 && foodPos == snakePos) {
+  if (foodPos == snakePos) {
+    // event.keyCode === 32 &&
     board[foodPos].classList.remove("food");
-    foodPos = Math.floor(Math.random() * 700);
+    var randomLocation = numArr[Math.floor(Math.random() * numArr.length)];
+    foodPos = randomLocation;
+
+    if (board[foodPos].classList.contains("tail")) {
+      board[foodPos].classList.remove("tail");
+    }
+
     board[foodPos].classList.add("food");
     score++;
     scoreSelector.innerHTML = score;
+    dropBomb();
+  } else if (board[snakePos].classList.contains("tail")) {
+    //event.keyCode === 32  &&  
+    alert("game over");
   }
 };
 
--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const container = document.getElementById("container")
 let board = []
 var snakePosVal;
 let score = 0;
+let numArr = []
+ 
+for(let i = 1; i < 784; i++) {
+    if (i % 28 === 0 || i % 28 === 27 || i <= 27 || i >756 && i <= 784) {continue;}
+    numArr.push(i)
+}
+
+
+console.log(numArr)
 
 let scoreSelector = document.querySelector("#score")
 
@@ -11,9 +20,11 @@ const createGrid = () => {
         const grid = document.createElement('div');
         container.appendChild(grid)
         board.push(grid)
-        
 
-        if (i <= 27 || i % 28 == 0 || i > 756 && i <= 784 || i % 28 === 27) {
+       
+         
+
+        if (i <= 27 || i % 28 === 0 || i > 756 && i <= 784 || i % 28 === 27) {
             board[i].classList.add('wall')
            
         } else {
@@ -32,16 +43,25 @@ let snakePos = 350;
 
 board[snakePos].classList.add('snake')
 
+
+
+
+const endGameWall = () => {
+    if (board[snakePos].classList.contains("wall")){
+        alert("game over")
+    }
+}
+
 const moveSnake = (event)  => {
     // board[snakePos].classList.remove('snake')
-  
-    
-
+      
     const down = () => {
     
         board[snakePos].classList.remove('snake')
         snakePos =  snakePos + snakePosVal
         board[snakePos].classList.add('snake')
+        eatFood()
+        endGameWall()
     }
 
        
@@ -49,38 +69,41 @@ const moveSnake = (event)  => {
         
         snakePosVal =  28;
         console.log('down')
-         var snakePosInterval = setInterval(down, 300)
+         var snakePosInterval = setInterval(down, 200)
+
+
          
         return clearInterval(snakePosInterval)
 
     }else if(event.keyCode === 13){
         snakePosVal = 1;
-        snakePosInterval = setInterval(down, 300)
+        snakePosInterval = setInterval(down, 200)
         
             
     }else if(event.keyCode === 39) {
         clearInterval(snakePosInterval)
         snakePosVal = 1;
         
-       snakePosInterval =  setInterval(down, 300)
+       snakePosInterval =  setInterval(down, 200)
        return clearInterval(snakePosInterval)
        
 
     } else if(event.keyCode === 38){
         console.log('up')
         snakePosVal= - 28
-        snakePosInterval = setInterval(down, 300)
+        snakePosInterval = setInterval(down, 200)
         return clearInterval(snakePosInterval)
        
     } else if(event.keyCode === 37){
         console.log('left')
         snakePosVal = -1;
-        snakePosInterval = setInterval(down, 300)
+        snakePosInterval = setInterval(down, 200)
         return clearInterval(snakePosInterval)
    }  
     
     
     board[snakePos].classList.add('snake')
+    
     return snakePos;
 } 
  
@@ -95,27 +118,44 @@ const drawFood = (event) => {
     }
 } 
 
+const dropBomb = () => {
+  board[snakePos].classList.add("tail") 
+
+}
 
 
 
 const eatFood = (event) => {
 
-    if(event.keyCode === 32 && foodPos == snakePos) {
-       
+    if( foodPos == snakePos) {
+        // event.keyCode === 32 &&
         
         board[foodPos].classList.remove("food")
     
-        foodPos = Math.floor((Math.random() * 700) )
+        let randomLocation = numArr[Math.floor(Math.random() * numArr.length)]
+
+        foodPos = randomLocation;
+
+        if(board[foodPos].classList.contains("tail")) {
+            board[foodPos].classList.remove("tail")
+        }
     
         board[foodPos].classList.add("food")
 
        score++
        scoreSelector.innerHTML = score;
 
+        dropBomb()
        
         
-    }  
+    }  else if(board[snakePos].classList.contains("tail")) {
+        //event.keyCode === 32  &&  
+        alert("game over")
+    }
 }   
+
+    
+
 
 
 document.addEventListener('keyup', eatFood)
